94f75a9193f43c5ce953e613d51155804785758c
dipskcoder <deepu.parmar3377@gmail.com>
2022-10-28 19:22:37 +0400

    theme update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const handleSelectTheme = (theme) => {
+    setTheme(theme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        className=""
+        onClick={() => {
+          handleSelectTheme("dark");
+        }}
+      >
+        Dark
+      </button>
+      <button
+        className=""
+        onClick={() => {
+          handleSelectTheme("light");
+        }}
+      >
+        Light
+      </button>
+      <button
+        className=""
+        onClick={() => {
+          handleSelectTheme("contrast");
+        }}
+      >
+        Contrast
+      </button>
+      <div className={"theme-" + theme}>
+        <input type="text" className="primary" placeholder="primary"></input>
+        <input
+          type="text"
+          className="secondary"
+          placeholder="secondary"
+        ></input>
+        <input type="checkbox" className="primary"></input> 
+        <label for="html" className="primary">
+          checkbox
+        </label>
+        <br />
+        <input type="radio" className="secondary"></input> {" "}
+        <label for="html" className="secondary">
+          Radio
+        </label>
+        <br />
+        <h2 className="primary">Heading tag primary</h2>
+        <h2 className="secondary">Heading tag secondary</h2>
+        <table className="tbl primary">
+          <tr>
+            <th>S.No</th>
+            <th>Name</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>sonali</td>
+            <td>09776548</td>
+          </tr>
+        </table>
+      </div>
+    </>
   );
 }
-
 export default App;
